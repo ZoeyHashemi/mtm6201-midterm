480958c3e28bde7656a5a32a7e034b3659e9b291
Zoey Hashemi <hash0169@algonquinlive.com>
2023-03-14 18:42:59 -0400

    beautify code

--- public/js/nav.js
+++ public/js/nav.js
@@ -4,38 +4,38 @@ const createNav =()=> {
    <div class="ribbon">
    <p>Extera 30% off on all products - Limited time offer!</p>
 </div>
-   <div class="nav">
-		<button class="nav-toggle">
-			<span class="hamburger"></span>
-			<span class="hamburger"></span>
-			<span class="hamburger"></span>
-		</button>
-		</button>
-			<img src="img/main-logo.png"  class="brand-logo" alt="AR Design logo">
-		<div class="nav-items">
-			<div class="search">
-				<img src="img/loupe.png " alt="user icon"></a>
-			</div>
-			<a href="#"><img src="img/user.png" alt="user icon"></a>
-			<a href="#"><img src="img/shopping-bag.png" alt=" shopping bag icon"></a>
-		</div>
-		</div>
-		<div class="hamburger">
-			<div class="line"></div>
-			<div class="line"></div>
-			<div class="line"></div>
-		</div>
-				<ul class="link-container">
-			<li class="link-item"><a href="index.html" class="link" aria-label= "Home">Home</a> </li>
-			<li class="link-item"><a href="#" class="link" aria-label="New Arrival">New Arrival</a></li>
-            <li class="link-item"><a href="#" class="link" aria-label="Best sellers">Best sellers</a></li>
-			<li class="link-item"><a href="#" class="link" aria-label="Clothing">Clothing</a></li>
-			<li class="link-item"><a href="#" class="link" aria-label="Dresses">Dresses</a></li>
-			<li class="link-item"><a href="#" class="link" aria-label="Accessories">Accessories</a></li>
-			<li class="link-item"><a href="#" class="link" aria-label="Collection">Collection</a></li>
-            <li class="link-item"><a href="about.html" class="link" aria-label="About">About</a></li>
-            <li class="link-item"><a href="contact.html" class="link" aria-label="Contact">Contact</a></li>
-		</ul>`;
+<div class="nav">
+   <button class="nav-toggle">
+   <span class="hamburger"></span>
+   <span class="hamburger"></span>
+   <span class="hamburger"></span>
+   </button>
+   </button>
+   <img src="img/main-logo.png"  class="brand-logo" alt="AR Design logo">
+   <div class="nav-items">
+      <div class="search">
+         <img src="img/loupe.png " alt="user icon"></a>
+      </div>
+      <a href="#"><img src="img/user.png" alt="user icon"></a>
+      <a href="#"><img src="img/shopping-bag.png" alt=" shopping bag icon"></a>
+   </div>
+</div>
+<div class="hamburger">
+   <div class="line"></div>
+   <div class="line"></div>
+   <div class="line"></div>
+</div>
+<ul class="link-container">
+   <li class="link-item"><a href="index.html" class="link" aria-label= "Home">Home</a> </li>
+   <li class="link-item"><a href="#" class="link" aria-label="Best sellers">Best sellers</a></li>
+   <li class="link-item"><a href="#" class="link" aria-label="Clothing">Clothing</a></li>
+   <li class="link-item"><a href="#" class="link" aria-label="Dresses">Dresses</a></li>
+   <li class="link-item"><a href="#" class="link" aria-label="Accessories">Accessories</a></li>
+   <li class="link-item"><a href="#" class="link" aria-label="Collection">Collection</a></li>
+   <li class="link-item"><a href="about.html" class="link" aria-label="About">About</a></li>
+   <li class="link-item"><a href="contact.html" class="link" aria-label="Contact">Contact</a></li>
+
+</ul>`;
    
    }
 
