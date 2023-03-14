9da75890b1ca4fd59680e1ade45d19637cb61d48
Zoey Hashemi <hash0169@algonquinlive.com>
2023-03-14 18:05:21 -0400

    first commit

---
+++ public/img/About_files/nav.js
@@ -0,0 +1,49 @@
+const createNav =()=> {
+   let nav = document.querySelector('.navbar');
+   nav.innerHTML = `
+   <div class="nav">
+		<button class="nav-toggle">
+			<span class="hamburger"></span>
+			<span class="hamburger"></span>
+			<span class="hamburger"></span>
+		</button>
+		</button>
+			<img src="img/main-logo.png"  class="brand-logo" alt="AR Design logo">
+		<div class="nav-items">
+			<div class="search">
+				<input type="text" class="search-box " placeholder="search product ">
+				<button class="search-btn">Go</button>
+			</div>
+			<a href="#"><img src="img/user.png" alt="user icon"></a>
+			<a href="#"><img src="img/shopping-bag.png" alt=" shopping bag icon"></a>
+		</div>
+		</div>
+		<div class="hamburger">
+			<div class="line"></div>
+			<div class="line"></div>
+			<div class="line"></div>
+		</div>
+				<ul class="link-container">
+			<li class="link-item"><a href="index.html" class="link" aria-label= "Home">Home</a> </li>
+			<li class="link-item"><a href="#" class="link" aria-label="New Arrival">New Arrival</a></li>
+            <li class="link-item"><a href="#" class="link" aria-label="Best sellers">Best sellers</a></li>
+			<li class="link-item"><a href="#" class="link" aria-label="Clothing">Clothing</a></li>
+			<li class="link-item"><a href="#" class="link" aria-label="Dresses">Dresses</a></li>
+			<li class="link-item"><a href="#" class="link" aria-label="Accessories">Accessories</a></li>
+			<li class="link-item"><a href="#" class="link" aria-label="Collection">Collection</a></li>
+            <li class="link-item"><a href="about.html" class="link" aria-label="About">About</a></li>
+            <li class="link-item"><a href="contact.html" class="link" aria-label="Contact">Contact</a></li>
+		</ul>`;
+   
+   }
+
+createNav(); 
+const navToggle = document.querySelector('.nav-toggle');
+const navItems = document.querySelector('.nav-items');
+const linkContainer = document.querySelector('.link-container');
+
+
+navToggle.addEventListener('click', () => {
+  navItems.classList.toggle('show');
+  linkContainer.classList.toggle('show');
+});
---
+++ public/js/footer.js
@@ -0,0 +1,69 @@
+const createFooter =() => {
+    let footer= document.querySelector('footer');
+    footer.innerHTML= `
+	    <div class="footer-content">
+					  <img src="img/light-logo.png"  class="logo" alt="Company Logo">
+			     <div class="footer-ul-container">
+				  <ul class="category">
+						<li class="category-title">Quick Links</li>
+						<li><a href="index.html" class="footer-link">Home</a></li>
+					  	<li><a href="#" class="footer-link">Products</a></li>
+						<li><a href="about.html" class="footer-link">About Us</a></li>
+						<li><a href="contact.html" class="footer-link">Contact</a></li>
+						<li><a href="#" class="footer-link">Blog</a></li>
+						<li><a href="#" class="footer-link">Help & FAQ's</a></li>
+						<li><a href="#" class="footer-link">Support</a></li>
+						<li><a href="#" class="footer-link">Online return service</a></li>
+						<li><a href="#" class="footer-link">Career</a></li>
+						<li><a href="#" class="footer-link">Privacy Policy</a></li>
+				 </ul >
+			      <ul class="category">
+						<li class="category-title">Services</li>
+						<li><a href="#" class="footer-link">Your Order</a></li>
+					  	<li><a href="#" class="footer-link">Shipping</a></li>
+						<li><a href="#" class="footer-link">Track a Package</a></li>
+						<li><a href="#" class="footer-link">Return & Exchange</a></li>
+						<li><a href="#" class="footer-link">Gift Cards</a></li>
+						<li><a href="#" class="footer-link">Check Gift Card Balance</a></li>
+						<li><a href="#" class="footer-link">Current Promotions</a></li>
+						<li><a href="#" class="footer-link">Return & Exchange</a></li>
+						<li><a href="#" class="footer-link">Return & Exchange</a></li>
+				 </ul >
+	 			</div>
+	   		</div>
+			   <p class="footer-title">About Us</p>
+			   <p class="info">It is a long established fact that a reader will be distracted
+				  by the readable content of a page when looking at its layout. 
+				 The point of using Lorem Ipsum is that it has a more-or-less 
+				 normal distribution of letters, as opposed to using ‘Content 
+				 here, content here’, making it look like readable English.
+				  Many desktop publishing packages and web page 
+				 editors now use Lorem Ipsum as their default model text, 
+			   web sites still in their infancy. </p>
+	  	 		<p class="info">Support Email- help@ardesign.com, customersupport@ardesign.com</p>
+				<p class="info">telephone- +1 343 100 0000, +1 343 200 0003</p>
+
+			<div class="footer-social-container">
+				<div>
+					<a href="#" class="social-link">terms & services</a>
+					<a href="#" class="social-link">privacy Page</a>
+				</div>
+				<div>
+
+					<a href="#" class="social-link"><img src="img/instagram.png" alt="Instagram icon"></a>
+					<a href="#" class="social-link"><img src="img/facebook.png" alt="Facebook icon"></a>
+					<a href="#" class="social-link"><img src="img/twitter.png" alt="Twitter icon"></a>
+					<a href="#" class="social-link"><img src="img/youtube.png" alt="Youtube icon"></a>
+
+				</div>
+
+					<p class="footer-credit"> &copy; 2023  AR Design Company, images from <a href="https://www.unsplash.com" class="unsplash"> Unsplash</a> </p>
+					<div class="back-button">
+				<a href="#top" id="back-btn" role="button">Back to Top</a>
+			</div>
+
+    `;
+}
+createFooter();
+
+
